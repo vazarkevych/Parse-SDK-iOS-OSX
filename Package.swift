cd381ed821fc2e0f889cddc8ebd4ecb511b9b4d1
Volodymyr Nazarkevych <volodymyr.nazarkevych@kevychsolutions.com>
2024-07-10 14:25:53 +0300

    update package file

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             exclude: ["Resources/Parse-tvOS.Info.plist", "Resources/Parse-iOS.Info.plist", "Resources/Parse-OSX.Info.plist", "Resources/Parse-watchOS.Info.plist"],
             resources: [.process("Resources")],
             publicHeadersPath: "Source",
-            cSettings: [.headerSearchPath("../Internal")]
+            cSettings: [.headerSearchPath("../Internal/..")]
         ),
         .target(
             name: "ParseLiveQuery",
